67dac3ece5d612729036feefee578e92a36ed40e
IronSid3e <omertahirustun05@hotmail.com>
2026-01-20 14:49:37 +0300

    feat: create SearchBar component with Feather icon and styling

---
+++ components/SearchBar.js
@@ -0,0 +1,30 @@
+import { StyleSheet, Text, View, TextInput } from "react-native";
+import React from "react";
+import Feather from "@expo/vector-icons/Feather";
+
+export default function SearchBar() {
+  return (
+    <View style={styles.backgroundStyle}>
+      <Feather style={styles.iconStyle} name="search" size={30} color="black" />
+      <TextInput style={styles.inputStyle} placeholder="Ara" autoCorrect={false} />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  backgroundStyle: {
+    backgroundColor: "lightgray",
+    flexDirection: "row",
+    margin: 10,
+    height: 50,
+    alignItems: "center",
+    borderRadius: 20,
+  },
+  iconStyle: {
+    marginHorizontal: 10,
+  },
+  inputStyle: {
+    flex: 1,
+    fontSize: 18,
+  },
+});
